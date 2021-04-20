84702e222803b47e236ba87f761d2eddc4464849
Jeet Gandhi <jeet.gandhi@zuhlke.com>
2021-04-20 15:43:59 +0800

    Initial Commit

---
+++ Carousel Demo/Carousel_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_DemoApp.swift
+//  Carousel Demo
+//
+//  Created by Jeet Gandhi on 20/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
